be956fcc05fb2857476b55ee2877c988b85f8536
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-11-14 20:28:01 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -3,5 +3,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request 
 
-st.title('Sismos anamelba')
+st.title('SISMOS')
+def download_data():
+  url = "https://www.datosabiertos.gob.pe/dataset/catalogo-sismico/resource/f9629081-0721-4101-a2c2-d0622e046f8b#{}"
+  filename = ""
+  urllib.request.urlretrieve(url,filename) 

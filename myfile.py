1c78b8721076680a58c2829e297ec18589373b72
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-11-18 09:00:34 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,14 +5,13 @@ import pandas as pd
 import numpy as np
 import urllib.request 
 
-#st.title('SISMOS')
-#def download_data():
-url = "https://github.com/aname1ba/proyecto/blob/main/Catalogo1960_2021.csv"
-#  filename = ""
-#  urllib.request.urlretrieve(url,filename) 
+
 
 #para gráficas de una línea
 st.title('Sismos')
-st.write('Analicemos los sismos')
+st.write('sismos')
+
+url = "https://github.com/aname1ba/proyecto/blob/main/Catalogo1960_2021.csv"
+
 st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
 

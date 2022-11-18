91fea11c919bdce37ec3a487064dbc1d0e7365ac
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-11-18 08:28:42 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -10,3 +10,9 @@ def download_data():
   url = "https://www.datosabiertos.gob.pe/dataset/catalogo-sismico/resource/f9629081-0721-4101-a2c2-d0622e046f8b#{}"
   filename = ""
   urllib.request.urlretrieve(url,filename) 
+
+#para gráficas de una línea
+st.title('Sismos')
+st.write('Analicemos los sismos')
+st.line_chart(data=datos, x='Date', y='Price')
+

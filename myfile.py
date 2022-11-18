7a28323f90a50b15e28688e34e42cf80b49cb9ec
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-11-18 10:55:24 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -11,7 +11,14 @@ datos = pd.read_csv(url, sep=',')
 
 #TITULO
 st.title('Sismos en el Perú desde 1960 hasta el 2021 según el IGP')
-st.write('sismos')
+st.write('A nivel mundial, el Perú es uno de los países de mayor potencial sísmico debido a que forma parte del denominado Cinturón de Fuego del Pacífico, región donde la Tierra libera más del 85% de la energía acumulada en su interior debido a los procesos de convección del manto.')
+
+st.subheader('Gráficas')
+
+st.subheader('Gráficas')
+
+
+st.subheader('Gráficas')
 
 st.subheader('Gráficas')
 

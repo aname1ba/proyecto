dc0e8dae267901146b507612ef90ccffd0906832
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-11-18 10:44:10 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -9,10 +9,15 @@ import urllib.request
 url = "https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo1960_2021.csv"
 datos = pd.read_csv(url, sep=',')
 
-#para gráficas de una línea
-st.title('Sismos')
+#TITULO
+st.title('Sismos en el Perú desde 1960 hasta el 2021 según el IGP')
 st.write('sismos')
 
+st.subheades('Gráficas')
+
+
+
+#GRÁFICAS
 st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
 
 print(datos)

3f91f657844cf22a0ddbd68cff45c1ee7a523fba
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-11-18 10:04:54 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,14 +5,14 @@ import pandas as pd
 import numpy as np
 import urllib.request 
 
-
+#URL
+url = "https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo1960_2021.csv"
+datos = pd.read_csv(url, sep=',')
 
 #para gráficas de una línea
 st.title('Sismos')
 st.write('sismos')
 
-url = "https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo1960_2021.csv"
-file = pd.read_csv(url, sep=',')
+st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
 
-
-
+print(datos)

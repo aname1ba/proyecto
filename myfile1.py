e8a7d96c5d7da84e5d1d40509eed93b27c0ee982
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 12:55:59 -0500

    Update myfile1.py

--- myfile1.py
+++ myfile1.py
@@ -7,7 +7,7 @@ import urllib.request
 #TABLA de todo los datos 
 st.title('Tabla de los sismos ocurridos en el Perú desde 1960 hasta el 2021 según el IGP')
 
-url = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo%20sismos%20-%20nuevo%20formato.csv'
+url = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo_sismos_nuevoformato.csv'
 datos = pd.read_csv(url, sep=',')
 st.table(datos)
 

22f0f7016d3cf7a8bf1bdcb1da2b49377ce6f042
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 12:39:24 -0500

    Update myfile1.py

--- myfile1.py
+++ myfile1.py
@@ -5,7 +5,7 @@ import pydeck as pdk
 import urllib.request
 
 #TABLA de todo los datos 
-st.title('Tabla de todos los datos')
+st.title('Tabla de los sismos en el Perú desde 1960 hasta el 2021 según el IGP')
 url1 = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo%20sismos%20-%20nuevo%20formato.csv'
 datos1 = pd.read_csv(url1, sep=',')
 st.table(datos1)

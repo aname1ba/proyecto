c965d03c7921ded5a67f1195ef65946cf59c391b
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-12-02 13:41:10 -0500

    Update myfile1.py

--- myfile1.py
+++ myfile1.py
@@ -7,6 +7,8 @@ import urllib.request
 #TABLA de todo los datos 
 st.title('Tabla de los sismos ocurridos en el Perú desde 1960 hasta el 2021 según el IGP')
 
+st.write('La tabla presentada fue obtenida del siguiente [enlace](https://www.datosabiertos.gob.pe/dataset/catalogo-sismico-1960-2021-igp)')
+
 url = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo_sismos_nuevoformato.csv'
 datos = pd.read_csv(url, sep=',')
 st.table(datos)

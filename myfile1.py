544857415bce1f6d692163fd20603ffa2a32c98f
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 12:38:31 -0500

    Update myfile1.py

--- myfile1.py
+++ myfile1.py
@@ -4,7 +4,8 @@ import numpy as np
 import pydeck as pdk
 import urllib.request
 
-#URL
+#TABLA de todo los datos 
+st.title('Tabla de todos los datos')
 url1 = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo%20sismos%20-%20nuevo%20formato.csv'
 datos1 = pd.read_csv(url1, sep=',')
 st.table(datos1)

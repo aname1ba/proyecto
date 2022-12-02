c2ffbb7fb78f040d4195dfb3d46798c439fef240
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 13:22:09 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -13,19 +13,15 @@ datos = pd.read_csv(url, sep=',')
 #TITULO
 st.title('Sismos en el Perú desde 1960 hasta el 2021 según el IGP')
 
-#INTRODUCCIÓN
+#INFORMACIÓN
 st.write('A nivel mundial, el Perú es uno de los países de mayor potencial sísmico debido a que forma parte del denominado *Cinturón de Fuego del Pacífico*, región donde la Tierra libera más del 85% de la energía acumulada en su interior debido a los procesos de convección del manto.')
 
 st.write('En este contexto, la actividad sísmica en torno de la placa del Pacífico, es debida a los diversos procesos de convergencia de placas con velocidades de hasta 8 cm/año. En América del Sur, en su borde occidental, son las placas de Nazca y Sudamericana las que convergen y desarrollan el proceso de subducción mediante el cual, la placa oceánica de Nazca se introduce por debajo de la continental o Sudamericana. Este proceso es el causante de la geodinámica activa del país y por ende, de una importante actividad sísmica, volcánica y efectos asociados.')
 
-#PRESENTACIÓN DE DATOS
-st.subheader('Presentación y análisis de la información')
-
 st.write('Para visualizar la tabla completa de todos los datos registrados por el IGP visita el siguiente [Link](https://aname1ba-proyecto-myfile1-m5nrdf.streamlit.app/).')
-st.write('La tabla de datos fue representada en el siguiente mapa:')
 
-#MAPA CON TODOS LOS DATOS 
-st.write('*Mapa 1. Sismos en el Perú desde 1960 hasta el 2021 según el IGP*')
+#MAPA todos los datos 
+st.subheader('Mapa 1. Sismos en el Perú desde 1960 hasta el 2021 según el IGP')
 df_mapa=pd.read_csv('Catalogo1960_2021.csv')
 df =  df_mapa.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
 st.map(df)
@@ -33,7 +29,7 @@ st.map(df)
 
 
 #GRÁFICA FECHA VS MAGNITUD
-st.write('Gráfica 1. Magnitud de sismos desde 1960 hasta 2021 contra Fecha UTC')
+st.subheader('Gráfica 1. Magnitud de sismos desde 1960 hasta 2021 contra Fecha UTC')
 st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
 print(datos)
 
@@ -42,32 +38,35 @@ st.write('Como se puede ver en la Gráfica 1, el 23 de Junio de 2001 a las 15 ho
 
 #RESUMEN de los datos
 st.subheader('Tabla 1. Resumen de 20 datos aleatorios')
-urlr = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Resumen%20catalogo%20sismos.csv'
+urlr = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Resumen_catalogo_sismos.csv'
 datosr = pd.read_csv(urlr, sep=',')
 st.table(datosr)
 
 
 #MAPA del resumen
 st.subheader('Mapa 2. Resumen de 20 datos aleatorios')
-df_mapa2 = pd.read_csv('Resumen%20catalogo%20sismos.csv')
+df_mapa2 = pd.read_csv('Resumen_catalogo_sismos.csv')
 df = df_mapa2.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
 
 
 #TABLA MAYOR
+st.subheader('Tabla 2. Los 10 primeros valores con mayor magnitud')
 urlM = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/MAYORvalor.csv'
 datosM = pd.read_csv(urlM, sep=',')
 st.table(datosM)
 
 #MAPA MAYOR
+st.subheader('Mapa 3. Los 10 primeros valores con mayor magnitud')
 
 
 #TABLA MENOR
+st.subheader('Tabla 3. Los 10 primeros valores con menor magnitud')
 urlm = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/MENORvalor.csv'
 datosm = pd.read_csv(urlm, sep=',')
 st.table(datosm)
 
 #MAPA MENOR
-
+st.subheader('Mapa 4. Los 10 primeros valores con menor magnitud')
 
 #TABLA DE DATOS
 url2= 'https://raw.githubusercontent.com/aname1ba/proyecto/main/2Catalogo1960_2021-lat%26lon%20-%20copia.csv'

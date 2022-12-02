fe911da4ce10be33d6318b5461ce608370135806
britneyvr <118000343+britneyvr@users.noreply.github.com>
2022-12-02 16:19:36 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -96,7 +96,7 @@ st.map(df4)
 
 #SUBHEADER 5: REFERENCIAS
 st.subheader('Referencias')
-st.write('1. Instituto Nacional de Defensa Civil (2006) Sismos ocurridos en el Perú a través del tiempo. Compendio Estadístico de Prevención y Atención de Desastres 2006. Recuperado de: https://www.indeci.gob.pe/compend_estad/2006/7_otras_estad/7.1_sismos/7.1.4_hist_sismos.pdf')
+st.write('[^1]: Instituto Nacional de Defensa Civil (2006) Sismos ocurridos en el Perú a través del tiempo. Compendio Estadístico de Prevención y Atención de Desastres 2006. Recuperado de: https://www.indeci.gob.pe/compend_estad/2006/7_otras_estad/7.1_sismos/7.1.4_hist_sismos.pdf')
 
 #AUTORES
 

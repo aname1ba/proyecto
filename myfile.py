20387ce01420efaab14592a52794fc4917fd9043
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 11:16:45 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -19,48 +19,12 @@ st.write('A nivel mundial, el Perú es uno de los países de mayor potencial sí
 st.write('En este contexto, la actividad sísmica en torno de la placa del Pacífico, es debida a los diversos procesos de convergencia de placas con velocidades de hasta 8 cm/año. En América del Sur, en su borde occidental, son las placas de Nazca y Sudamericana las que convergen y desarrollan el proceso de subducción mediante el cual, la placa oceánica de Nazca se introduce por debajo de la continental o Sudamericana. Este proceso es el causante de la geodinámica activa del país y por ende, de una importante actividad sísmica, volcánica y efectos asociados.')
 
 
-
-
-
-#MAPA
+#MAPA todos los datos 
 st.subheader('Datos en el mapa')
-
 df_mapa=pd.read_csv('Catalogo1960_2021.csv')
 df =  df_mapa.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
 st.map(df)
 
-url2= 'https://raw.githubusercontent.com/aname1ba/proyecto/main/2Catalogo1960_2021-lat%26lon%20-%20copia.csv'
-df=pd.read_csv(url2)
-
-#mapa profesor
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=38.904,
-        longitude=-77.016,
-        zoom=5,
-        pitch=70,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=df,
-           get_position='[lon, lat]',
-           radius=10, #acumula los valores
-           elevation_scale=9, #da una escala de 0 a 9
-           elevation_range=[0, 22711], # total de posibles valores
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
 
 #GRÁFICA FECHA VS MAGNITUD
 st.write('Gráfica 1. Magnitud de sismos desde 1960 hasta 2021 contra Fecha UTC')
@@ -69,10 +33,25 @@ print(datos)
 
 st.write('Como se puede ver en la Gráfica 1, el 23 de Junio de 2001 a las 15 horas 33 minutos, ocurrio un terremoto destructor de una magnitud de 8.4 que afectó el Sur del Perú, particularmente los Departamentos de Moquegua, Tacna y Arequipa. Este sismo tuvo características importantes entre las que se destaca la complejidad de su registro y ocurrencia. El terremoto ha originado varios miles de post-sacudidas o réplicas y alcanzó una intensidad máxima de VIII. Las localidades más afectadas por el terremoto fueron las ciudades de Moquegua, Tacna, Arequipa, Valle de Tambo, Caravelí, Chuquibamba, Ilo, algunos pueblos del interior y Camaná por el efecto del Tsunami.')
 
-#GRÁFICA HORA VS MAGNITUD
-st.write('Gráfica 2. Magnitud de sismos desde 1960 hasta 2021 contra Hora UTC')
-st.line_chart(data=datos, x='HORA_UTC', y='MAGNITUD')
-print(datos)
+
+#RESUMEN de los datos
+
+
+
+#TABLA MAYOR
+urlM = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/MAYORvalor.csv'
+datosM = pd.read_csv(urlM, sep=',')
+st.table(datosM)
+
+#MAPA MAYOR
+
+
+#TABLA MENOR
+urlm = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/MENORvalor.csv'
+datosm = pd.read_csv(urlm, sep=',')
+st.table(datosm)
+
+#MAPA MENOR
 
 
 #TABLA DE DATOS

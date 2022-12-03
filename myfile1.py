71f3d0be4cdf654fbbae3a6f47ace26311330b56
aname1ba <117988405+aname1ba@users.noreply.github.com>
2022-12-02 22:34:44 -0500

    Update myfile1.py

--- myfile1.py
+++ myfile1.py
@@ -8,7 +8,7 @@ import urllib.request
 st.title('Tabla de los sismos ocurridos en el Perú desde 1960 hasta el 2021 según el IGP')
 
 st.write('El presente catálogo ha sido elaborado por el Instituto Geofísico del Perú (IGP), para descargar el archivo original entrar al siguiente [enlace.](https://www.datosabiertos.gob.pe/dataset/catalogo-sismico-1960-2021-igp)')
-st.buttom('enlace')
+st.button('enlace')
 
 url = 'https://raw.githubusercontent.com/aname1ba/proyecto/main/Catalogo_sismos_nuevoformato.csv'
 datos = pd.read_csv(url, sep=',')
